e993a4869563bfae690665f7de020a299f301b4e
itteammobilogi <it@mobilogi.com>
2025-09-20 21:56:14 +0530

    installed package of sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import SitemapPlugin from 'vite-plugin-sitemap';
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,6 +15,10 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    SitemapPlugin({
+      hostname: 'https://www.venstyler.com',  
+      exclude: ['/excluded-page', '/another-page-to-exclude'],
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
