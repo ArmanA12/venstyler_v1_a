c108aab618e8f3a608e29b5d8a71563acce087e9
Arman Shekh <97970564+ArmanA12@users.noreply.github.com>
2025-09-22 17:12:41 +0530

    Update use-toast.ts

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -170,7 +170,6 @@ function toast({ ...props }: Toast) {
 
 function useToast() {
   const [state, setState] = React.useState<State>(memoryState)
-
   React.useEffect(() => {
     listeners.push(setState)
     return () => {
