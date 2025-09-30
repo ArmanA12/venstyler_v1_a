222bacb1b39747c7a0157295b899ef0a2b917863
Arman Shekh <97970564+ArmanA12@users.noreply.github.com>
2025-09-30 14:29:22 +0530

    Update Spline.tsx

--- src/components/Spline.tsx
+++ src/components/Spline.tsx
@@ -3,11 +3,9 @@ import Spline from '@splinetool/react-spline';
 export default function SplineWave() {
   return (
     <main>
-      
       <Spline
         scene="https://prod.spline.design/JEEY-whUDk-y-l4e/scene.splinecode" 
       />
-      
     </main>
   );
 }
