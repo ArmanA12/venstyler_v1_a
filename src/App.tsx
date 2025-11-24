c4940340cab8c54ccce93d62aef827fc14fa992e
finesse-mumbai <web@finesse.co.in>
2025-11-24 12:42:20 +0530

    chnage

--- src/App.tsx
+++ src/App.tsx
@@ -91,7 +91,7 @@ export default function App() {
             <Toaster />
             <Sonner />
 
-            {/* socket + toast + notification write lives **inside** providers */}
+           
             <NotificationsSocketBridge userId={userId} />
 
             <BrowserRouter>
